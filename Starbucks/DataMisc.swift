284a248e94231ac1a00f2c95e894556f25b7cc58
enea_dume <eneadume@gmail.com>
2020-10-06 14:58:55 +0200

    changed google api key

--- Starbucks/DataMisc.swift
+++ Starbucks/DataMisc.swift
@@ -5,5 +5,5 @@
 //  Created by User on 4/27/19.
 //  Copyright © 2019 Enea Dume. All rights reserved.
 //
-public let API_KEY = "AIzaSyAbRp5tq_R2fVZFuu0JRTCD0-Vf-g4PD-E"
+public let API_KEY = "Your Google Api Key"
 public let LOCAL_STORES_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=starbucks+seattle&sensor=false&key=\(API_KEY)"
